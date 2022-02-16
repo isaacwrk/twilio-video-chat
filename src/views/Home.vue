bda6840eab3a21d9f0b989b058ff5dcd22923dc5
isaacwrk <isaacsouzawrk@gmail.com>
2022-02-16 14:55:00 -0300

    feature: implement tailwindcss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="bg-red-200 text-500 justify-center flex">
+        alou
+    </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
 
 @Options({
-  components: {
-    HelloWorld,
-  },
+    components: {
+    },
 })
 export default class Home extends Vue {}
 </script>
